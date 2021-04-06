8078bc594918dcecae6849307ba09ca722771d2e
SwethaSankar29 <64885201+SwethaSankar29@users.noreply.github.com>
2021-04-06 14:49:38 +0530

    Add files via upload

--- src/components/PureComponent.js
+++ src/components/PureComponent.js
@@ -3,13 +3,9 @@ import React from "react";
 export default class PureComponent extends React.PureComponent {
   render() {
     const { userName } = this.props;
-    const changeUserName = () => {
-      alert("props changed");
-    };
-
     return (
       <div>
-        <h1> Hi {userName} </h1> {changeUserName()}{" "}
+        <h1> Hi {userName} </h1> {alert("props changed")}{" "}
       </div>
     );
   }
